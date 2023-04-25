ad12f023d9b53c5aa25bee2981c29466936b1e06
umair shah <umairshah14@hotmail.com>
2023-04-25 01:19:22 +0100

    memoji added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,37 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
-
+import Me from "../assets/memoji.png";
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-black">
       {/* Container */}
 
-      <div className="max-w[1000px] mx-auto px-8 flex flex-col justify-center h-full lg:ml-36">
-        <p className="text-yellow-500 text-2xl">Hi, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-white">Umair Shah</h1>
-        <h2 className="text-3xl sm:text-6xl font-bold text-yellow-700">A Junior Full Stack Developer</h2>
-        <p className="text-slate-400 py-4 max-w-[700px] ">I've been coding for just under 2 years now, im passionate about learning, enthusiastic about tech and hungry for an opportunity to show what i can bring to the table</p>
-        <div>
-            <button className="text-white group border-2 px-4 py-3 my-2 flex items-center rounded-lg hover:bg-slate-600 hover:border-slate-600"> View work 
-            <span className="group-hover:rotate-90 duration-300">
-            <HiArrowNarrowRight className="ml-3"/>
-            </span>
-            </button>
+      <div className="max-w[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <div className="flex items-center">
+          <img src={Me} alt="" className="mr-20 ml-[10%] hidden lg:flex" />
+          <div>
+            <p className="text-yellow-500 text-2xl">Hi, my name is</p>
+            <h1 className="text-4xl sm:text-7xl font-bold text-white">
+              Umair Shah
+            </h1>
+            <h2 className="text-3xl sm:text-6xl font-bold text-yellow-700">
+              A Junior Full Stack Developer
+            </h2>
+            <p className="text-slate-400 py-4 max-w-[700px] ">
+              I've been coding for just under 2 years now, im passionate about
+              learning, enthusiastic about tech and hungry for an opportunity to
+              show what i can bring to the table
+            </p>
+            <div>
+              <button className="text-white group border-2 px-4 py-3 my-2 flex items-center rounded-lg hover:bg-neutral-800 hover:border-neutral-800">
+                {" "}
+                View work
+                <span className="group-hover:rotate-90 duration-300">
+                  <HiArrowNarrowRight className="ml-3" />
+                </span>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-black text-slate-200">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-neutral-900 text-slate-200">
       <div>
         <img src={Logo} alt="my logo" style={{ width: "50px" }} />
       </div>
@@ -27,7 +27,7 @@ const Navbar = () => {
       </div>
 
       {/* MOBILE MENU */}
-      <ul className={!nav ? ' hidden' : "absolute top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center "}>
+      <ul className={!nav ? ' hidden' : "absolute top-0 left-0 w-full h-screen bg-neutral-900 flex flex-col justify-center items-center "}>
         <li className="py-6 text-4xl">Home</li>
         <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Skills</li>
